c90ec1f54147538cdf1306559bd762fea17e87cc
Alexandre Jourdain <solweigdev@gmail.com>
2021-05-20 20:43:57 +0200

    add script

--- script.js
+++ script.js
@@ -16,15 +16,3 @@ WA.onChatMessage((message => {
     	WA.sendChatMessage('Je ne comprend pas', 'Mr Bundle');
     }
 }));
-
-WA.onEnterZone('myZone', () => {
-	WA.disablePlayerControls();
-	WA.openPopup("messagedavid", 'David tu dois faire les maquettes', ({
-		label: "Ok",
-		className: "primary",
-		callback: (popup) => {
-			WA.restorePlayerControls();
-			popup.close();
-		}
-	})
-})
